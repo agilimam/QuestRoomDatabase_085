a8b512ad328bce4f7dbb68b0d24a7a397bbf3a7c
agilimam <agilimamalhafizi04@gmail.com>
2024-12-16 19:31:18 +0700

    menambahkan beberapa operasi untuk mengelola data mahasiswa

--- app/src/main/java/com/example/pertemuan10pam/data/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10pam/data/repository/RepositoryMhs.kt
@@ -1,7 +1,14 @@
 package com.example.pertemuan10pam.data.repository
 
+import androidx.room.Update
 import com.example.pertemuan10pam.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun UpdateMhs(mahasiswa: Mahasiswa)
 }
