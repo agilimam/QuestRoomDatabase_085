e9ac05c8d47188e6fc0014725c16670fd09d2deb
agilimam <agilimamalhafizi04@gmail.com>
2024-12-16 19:47:34 +0700

    menambahkan kode untuk menghubungkan MahasiswaDao dengan operasi yang didefinisikan dalam RepositoryMhs

--- app/src/main/java/com/example/pertemuan10pam/data/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10pam/data/repository/RepositoryMhs.kt
@@ -10,5 +10,5 @@ interface RepositoryMhs {
     fun getAllMhs(): Flow<List<Mahasiswa>>
     fun getMhs(nim: String): Flow<Mahasiswa>
     suspend fun deleteMhs(mahasiswa: Mahasiswa)
-    suspend fun UpdateMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
