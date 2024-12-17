d0cfc5a0f4f90562d3b2b0b3233db251995236d4
agilimam <agilimamalhafizi04@gmail.com>
2024-12-17 07:30:40 +0700

    menambahkan object destinasi home

--- app/src/main/java/com/example/pertemuan10pam/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan10pam/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,7 @@ package com.example.pertemuan10pam.ui.navigation
 interface AlamatNavigasi {
     val route : String
 }
+
+object DestinationHome : AlamatNavigasi{
+    override val route = "home"
+}
