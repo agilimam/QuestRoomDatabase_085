282a5106b10e6d0b9a8093ccbf593e7f8a12adaf
agilimam <agilimamalhafizi04@gmail.com>
2024-12-17 08:11:29 +0700

    membuat sebuah fungsi untuk memindahkan data dari entity ke ui

--- app/src/main/java/com/example/pertemuan10pam/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10pam/ui/viewmodel/DetailMhsViewModel.kt
@@ -3,10 +3,10 @@ package com.example.pertemuan10pam.ui.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan10.viewmodel.MahasiswaEvent
+import com.example.pertemuan10pam.data.entity.Mahasiswa
 import com.example.pertemuan10pam.data.repository.RepositoryMhs
 import com.example.pertemuan10pam.ui.navigation.DestinasiDetail
 import kotlinx.coroutines.flow.StateFlow
-
 
 
 data class DetailUiState(
@@ -20,4 +20,19 @@ data class DetailUiState(
         get() = detailUiEvent == MahasiswaEvent()
     val isUiEvenNotEmpty: Boolean
         get() = detailUiEvent != MahasiswaEvent()
+}
+
+//Data class untuk menampung data yang akan di tampilkan di UI
+
+// Memindahkan data dari entity ke ui
+
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
